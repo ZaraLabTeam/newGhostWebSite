1ed3fc4c7a0f89515a7c5a2c6a4d64f98eac04c9
xzq70r4 <v.dimov@cnapsys.com>
2016-04-14 19:52:12 +0300

    fix youtube player

--- content/themes/zaralab/src/js/src/youtube-videos.js
+++ content/themes/zaralab/src/js/src/youtube-videos.js
@@ -67,23 +67,11 @@
 
   function setPlayerWithLinkData () {
     /*jshint validthis:true*/
+    var iframe = '<iframe id="youtube" width="100%" height="100%" src="//www.youtube.com/embed/'+
+                  $(this).attr('data-youtube-id')+
+                  '?autoplay=1" frameborder="0" allowfullscreen></iframe></div>';
 
-    // Grab the video ID from the element clicked
-    var id = $(this).attr('data-youtube-id');
-
-    // Autoplay when the modal appears
-    // Note: this is intetnionally disabled on most mobile devices
-    // If critical on mobile, then we need to brainstorm a way, or not use YouTube?
-    var autoplay = '?autoplay=1';
-
-    // Don't show the 'Related Videos' when the video ends
-    var related_no = '&rel=0';
-
-    // String the ID and param variables together
-    var src = '//www.youtube.com/embed/'+id+autoplay+related_no;
-
-    // Set the source on the iframe template inside the video modal
-    $("#youtube").attr('src', src);
+    $('#video-modal-content').append(iframe);
     return false;
   }
 
@@ -96,13 +84,12 @@
       });
 
       // Close and Reset the Video Modal
-      $('body').on('click', '.close-video-modal, .video-modal .overlay', function(event) {
+      $('#close-video').on('click', function(event) {
+        console.log('click');
           event.preventDefault();
 
           $("body").removeClass("show-video-modal");
-
-          // reset the source attribute for the iframe template - kills the video
-      $("#youtube").attr('src', '');
+          $('#youtube').remove();
       });
   }
 
