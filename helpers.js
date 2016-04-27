0f1ee54b5e90d19becabad2239c2630049783969
xzq70r4 <xzq70r4@gmail.com>
2016-04-27 16:35:16 +0300

    Add hbs helper for add one year plus to post date and fix response for facebook link for post

--- helpers.js
+++ helpers.js
@@ -5,7 +5,6 @@ var downsize = require('./node_modules/ghost/node_modules/downsize');
 module.exports = function() {
   hbs.registerHelper('short_content', function() {
 
-
     excerpt = String(this.html);
     // Strip inline and bottom footnotes
     excerpt = excerpt.replace(/<span class="caption">.*<\/span>/gi, '');
@@ -16,7 +15,23 @@ module.exports = function() {
     excerpt = excerpt.replace(/<\/?[^>]+>/gi, '');
     excerpt = excerpt.split('.');
     excerpt = excerpt[0].replace(/(\r\n|\n|\r)+/gm, ' ');
-    return excerpt + '.'
+    return excerpt + '.';
+  });
+
+  hbs.registerHelper("formatDate", function(options){
+
+    var postDate = options.fn(this),
+        postDateLastChar,
+        postDateWithoutLastChar,
+        postDateWithoutNewLastChar,
+        postNewDate;
+
+    postDateLastChar = postDate.slice(-1);
+    postDateWithoutLastChar = postDate.slice(0, postDate.length - 1);
+    postDateWithoutNewLastChar = parseInt(postDateLastChar) + 1;
+
+    postNewDate = postDateWithoutLastChar + postDateWithoutNewLastChar;
+
+    return new hbs.SafeString(postNewDate);
   });
 };
-
