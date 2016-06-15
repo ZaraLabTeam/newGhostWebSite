9c82eea4694691f1b2e61929e2bf154bf6e4cb85
Peter Velkov <kidroca@gmail.com>
2016-06-15 12:01:01 +0300

    Added a script for automatic merge to cloudstrap The script can be used from the master branch of the cloudstrap repository

---
+++ cloud-scirpt.sh
@@ -0,0 +1,33 @@
+echo "########################################################################"
+echo "################## CLOUDSTRAP AUTO MERGE SCRIPT ########################"
+echo "########################################################################"
+echo ""
+
+read -p "Enter the remote name for the github remote: " github
+
+echo "Fetching data from github"
+git fetch $github
+
+echo "EXcluding changes to content/themes/zaralab/.gitignore and merging"
+git merge --no-ff --no-commit $github/master
+git checkout content/themes/zaralab/.gitignore
+
+echo "Committing changes"
+git commit -m "Merged github repository to cloudstrap"
+
+echo "Starting gulp task to build assets"
+cd content/themes/zaralab/
+gulp build
+
+echo "Committing changes"
+cd ../../../
+git add .
+git commit -m "Rebuilt the assets folder"
+
+echo ""
+echo "########################################################################"
+echo ""
+
+echo "All done..." 
+echo ""
+echo "...You still need to push the changes with 'git push'"
